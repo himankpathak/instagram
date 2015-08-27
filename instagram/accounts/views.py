32f3aee063e473b06b54a446714f264d92990c37
Ben <iglesia.ben@gmail.com>
2015-08-27 16:32:00 +0800

    Update base.html

--- instagram/accounts/views.py
+++ instagram/accounts/views.py
@@ -1,23 +1,32 @@
 from __future__ import absolute_import
 
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.views import generic
 from django.core.urlresolvers import reverse_lazy
 
+from braces import views
+
 from .forms import SignUpForm, LoginForm
 
 
-class SignUpView(generic.CreateView):
+class SignUpView(views.AnonymousRequiredMixin,
+                 views.FormValidMessageMixin,
+                 generic.CreateView):
     form_class = SignUpForm
+    form_valid_message = 'Thanks for signing up, go ahead and login.'
     model = User
     template_name = 'accounts/signup.html'
 
 
-class LoginView(generic.FormView):
+class LoginView(views.AnonymousRequiredMixin,
+                views.FormValidMessageMixin,
+                generic.FormView):
     form_class = LoginForm
+    form_valid_message = 'You\'re logged in now.'
     success_url = reverse_lazy('home')
     template_name = 'accounts/login.html'
 
@@ -33,6 +42,7 @@ class LoginView(generic.FormView):
             return self.form_invalid(form)
 
 
+@login_required
 def logout_view(request):
     logout(request)
     messages.success(request, 'You\'ve been logged out. Come back soon!')
