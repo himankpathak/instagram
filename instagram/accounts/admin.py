cbb7f2bf5de447a58928392dd1fd9cdab87f90e5
Ben <iglesia.ben@gmail.com>
2015-08-28 22:51:54 +0800

    Update user model.

--- instagram/accounts/admin.py
+++ instagram/accounts/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from accounts.models import User
+from .forms import UserCreationForm, UserChangeForm
+
+
+class UserAdmin(UserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('username', 'email', 'name', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name',)}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+    add_fieldsets = ((None, {
+        'classes': ('wide',),
+        'fields': ('email', 'date_of_birth', 'password1', 'password2')}),
+    )
+    search_fields = ('username',)
+    ordering = ('username',)
+    filter_horizontal = ()
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
--- instagram/accounts/forms.py
+++ instagram/accounts/forms.py
@@ -1,7 +1,52 @@
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit
+
+from accounts.models import User
+
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password',
+                                widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation',
+                                widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('Passwords don\'t match')
+        return password2
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ('username',
+                  'email',
+                  'password',
+                  'name',
+                  'is_active',
+                  'is_admin')
+
+    def clean_password(self):
+        return self.initial['password']
 
 
 class SignUpForm(UserCreationForm):
@@ -11,6 +56,7 @@ class SignUpForm(UserCreationForm):
         self.helper = FormHelper()
         self.helper.layout = Layout(
             'username',
+            'email',
             'password1',
             'password2',
             Submit('signup', 'Sign up', css_class='btn primary')
