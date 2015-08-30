f58a9d2b212355186393e83c9aeec240c1048c02
Ben <iglesia.ben@gmail.com>
2015-08-30 18:57:12 +0800

    Update main url for media files.

--- instagram/instagram/urls.py
+++ instagram/instagram/urls.py
@@ -1,5 +1,9 @@
+from __future__ import absolute_import
+
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import HomePageView
 
@@ -8,4 +12,4 @@ urlpatterns = [
     url(r'^u/', include('accounts.urls', namespace='accounts')),
     url(r'^p/', include('posts.urls', namespace='posts')),
     url(r'^admin/', include(admin.site.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
