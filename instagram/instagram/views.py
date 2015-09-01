997c4ee5afe9cb6874e58c2a9ecbaad1ef209e96
Ben <iglesia.ben@gmail.com>
2015-09-01 22:42:00 +0800

    Update wall.

--- instagram/instagram/views.py
+++ instagram/instagram/views.py
@@ -1,8 +1,22 @@
+from django.db.models import Q
 from django.views import generic
 
+from accounts.models import Connection
 from posts.models import Post
 
 
 class HomePageView(generic.ListView):
     model = Post
     template_name = 'home.html'
+
+    def get_queryset(self):
+        connections = Connection.objects.filter(
+            follower__username=self.request.user
+        )
+
+        following = [connection.following for connection in connections]
+
+        return Post.objects.filter(
+            Q(author__in=following) |
+            Q(author__username=self.request.user)
+        )
