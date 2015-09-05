5b7f1d721199e27e6f0cb3254e7a3e20cceb5db9
Ben <iglesia.ben@gmail.com>
2015-09-05 23:06:01 +0800

    Add likes to home view.

--- instagram/instagram/views.py
+++ instagram/instagram/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from accounts.models import Connection
-from posts.models import Post
+from posts.models import Post, Like
 
 
 class HomePageView(generic.ListView):
@@ -16,10 +16,15 @@ class HomePageView(generic.ListView):
             follower__username=self.request.user
         ).values_list('following').order_by('date_created')
 
-        return Post.objects.filter(
+        posts = Post.objects.filter(
             Q(author__in=following) |
             Q(author__username=self.request.user)
         )
+
+        return {
+            post: Like.objects.filter(post=post).count()
+            for post in posts
+        }
 
 
 def handler404(request):
